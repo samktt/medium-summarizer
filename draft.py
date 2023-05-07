76c99a22ec9280346d10f11b87eb4a6bd100a02c
Sam Toussi <55114165+samktt@users.noreply.github.com>
2023-05-06 17:34:38 -0700

    refactor and fix issue with author dynamic content

--- draft.py
+++ draft.py
@@ -4,34 +4,75 @@ from bs4 import BeautifulSoup as bs
 import re
 import time
 
-#Setting the URL to Medium's homepage
+# Set the URL to Medium's homepage
 url = "https://medium.com/"
 
-#Sending a GET request to the URL and parsing the HTML response using Beautiful Soup
+# Send a GET request to the URL
 res = requests.get(url)
+
+# Parse the HTML response using Beautiful Soup
 soup = bs(res.text, "html.parser")
 
-#Finding the section with class "pw-homefeed", which contains the articles
+# Find the section with class "pw-homefeed", which contains the articles
 news_article = soup.find("section", {"class": "pw-homefeed"})
 
-#Finding all the 'a' elements with 'href' attributes containing an https URL, and storing them in a set to remove duplicates
-articles = set(map(lambda x: x.get('href'), news_article.find_all("a", attrs={'href': re.compile("^https://")})))
+# Define a function to extract 'href' attribute from an 'a' element
+def extract_href(a_element):
+    return a_element.get('href')
 
-#Iterating through the set of articles
+# Find all the 'a' elements with 'href' attributes containing an https URL
+a_elements = news_article.find_all("a", attrs={'href': re.compile("^https://")})
+
+# Extract the URLs (href attributes) from the 'a' elements using the 'extract_href' function
+article_urls = map(extract_href, a_elements)
+
+# Convert the list of URLs to a set to remove duplicates
+unique_articles = set(article_urls)
+
+
+
+# Function to fetch and parse article data from a given URL
+def get_article_data(article_url):
+    # Send a GET request to the article URL
+    response = requests.get(article_url)
+    # Parse the HTML response using Beautiful Soup
+    soup = bs(response.text, "html.parser")
+
+    # Find the title, description paragraphs, and author elements in the parsed HTML
+    title = soup.find("h1", {"class": "pw-post-title"})
+    descs = soup.find_all("p", {"class": "pw-post-body-paragraph"})
+    author = soup.find("div", {"class": "pw-author"})
+
+    # Extract text from the elements found, handling cases when elements are not found
+    title_text = title.text if title else ''
+    descs_text = [desc.text for desc in descs]
+    author_text = author.text if author else ''
+
+    # Return the extracted data in a dictionary
+    return {
+        'url': article_url,
+        'title': title_text,
+        'descs': descs_text,
+        'author': author_text
+    }
+
+# Iterate through the set of unique article URLs
 for article in articles:
-    url = article
-    print(url)
-    
-    # Send a GET request to the article URL and parse the HTML response
-    res = requests.get(article)
-    soup = bs(res.text, "html.parser")
-    
-    # Extract the title, description paragraphs, and author of the article
-    title = (soup.find("h1", {"class": "pw-post-title"}).text if soup.find("h1", {"class": "pw-post-title"}) else '')
-    descs = list(map(lambda x: x.text, soup.find_all("p", {"class": "pw-post-body-paragraph"})))
-    author = (soup.find("div", {"class": "pw-author"}).text if soup.find("div", {"class": "pw-author"}) else '')
-    
-    # Print the extracted information if it's available
-    if author and title and descs:
-        print(f"Author: {author}\nTitle: {title}\nDesc len: {len(descs)}\n")
-    time.sleep(5)
+    try:
+        # Fetch and parse the article data
+        article_data = get_article_data(article)
+
+        # Check if the required data (author, title, descriptions) is available
+        if article_data['author'] and article_data['title'] and article_data['descs']:
+            # Print the extracted information in a formatted manner
+            print(f"URL: {article_data['url']}")
+            print(f"Author: {article_data['author']}")
+            print(f"Title: {article_data['title']}")
+            print(f"Description count: {len(article_data['descs'])}\n")
+
+        # Add a delay between requests to avoid overloading the server
+        time.sleep(2)
+
+    # Handle any exceptions that may occur during the processing of an article
+    except Exception as e:
+        print(f"Error while processing article {article}: {e}")
